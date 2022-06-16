5172d0a26bf2aacb5ef308219b4f33983650d4cb
lxcao <caolingxin@hotmail.com>
2022-06-16 10:43:40 +0800

    enable log

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # oci-servicemesh-tutorial
 oci service mesh tutorial
+
+This repository are example scripts to work with the Oracle Cloud Infrastructure Service Mesh
+Following the OCI document: https://docs.oracle.com/en-us/iaas/Content/service-mesh-tutorials/service-mesh-overview/00-overview.htm
--- provision.sh
+++ provision.sh
@@ -18,3 +18,10 @@ kubectl get svc bookinfo-ingress -n bookinfo
 
 curl http://<EXTERNAL-IP>
 
+#Setup Logging Service to Store your Logs
+oci logging log-group create --compartment-id ocid1.compartment.oc1..aaaaaaaayjcsmu5ii7ac3kncp5qlbsslaj7irtc3mo4oco22w7ucsiq3atmq --display-name bookinfo --profile specialist2 --auth security_token
+
+oci logging log create --log-group-id ocid1.loggroup.oc1.ap-singapore-1.amaaaaaacuco5yqahgngmt5xkqsh5vr32uf4r3eexdrgfbrzb6hdiskrfs6q --display-name bookinfo-logs --log-type custom --profile specialist2 --auth security_token
+
+oci logging agent-configuration create --compartment-id ocid1.compartment.oc1..aaaaaaaayjcsmu5ii7ac3kncp5qlbsslaj7irtc3mo4oco22w7ucsiq3atmq --is-enabled true --service-configuration file:///Users/caolingxin/documents/workspaces/oci-projects/oci-servicemesh-tutorial/logconfig.json --display-name bookInfoLoggingAgent --description "Custom agent config for mesh" --group-association '{"groupList": ["ocid1.dynamicgroup.oc1..aaaaaaaaosz4fhyqdms3ze5wljrbyq7q7ueiglodnx6heqs2ansb33cdt2bq"]}'  --profile specialist2 --auth security_token
+
